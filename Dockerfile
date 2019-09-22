0c5b52804b689fca74018472854c2407beb85fdf
Ybrin <koray@koska.at>
2019-09-22 10:37:42 +0200

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:12.10.0-alpine
+
+LABEL "version"="0.1.0"
+LABEL "repository"="https://github.com/Ybrin/secure-actions-webhook"
+LABEL "homepage"="https://github.com/Ybrin/secure-actions-webhook"
+LABEL "maintainer"="Koray Koska <koray@koska.at>"
+LABEL "com.github.actions.name"="Secure Actions Webhook"
+LABEL "com.github.actions.description"="Post data and an hmac signature to an endpoint"
+LABEL "com.github.actions.icon"="message-square"
+LABEL "com.github.actions.color"="gray-dark"
+
+# Add the entry point
+ADD main.js /main.js
+ADD package.json /package.json
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+WORKDIR /
+
+RUN npm install
+
+# Load the entry point
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ main.js
@@ -0,0 +1,62 @@
+const request = require("request");
+const crypto = require("crypto");
+
+const hmacSecret = process.env.HMAC_SECRET;
+if (!hmacSecret || hmacSecret === "" || hmacSecret.trim() === "") {
+  console.warn(
+    "The hmac secret seems empty. This doesn't seem like what you want."
+  );
+}
+if (hmacSecret.length < 32) {
+  console.warn(
+    "The hmac secret seems week. You should use at least 32 secure random hex chars."
+  );
+}
+
+const createHmacSignature = body => {
+  const hmac = crypto.createHmac("sha1", hmacSecret);
+  const bodySignature = hmac.update(JSON.stringify(body)).digest("hex");
+
+  return `sha1=${bodySignature}`;
+};
+
+function isJsonString(str) {
+  try {
+    const json = JSON.parse(str);
+    return typeof json === "object";
+  } catch (e) {
+    return false;
+  }
+}
+
+const url = process.env.REQUEST_URL;
+const data = {
+  data: isJsonString(process.env.REQUEST_DATA) ? JSON.parse(process.env.REQUEST_DATA) : process.env.REQUEST_DATA
+};
+
+const signature = createHmacSignature(data);
+
+request(
+  {
+    method: "POST",
+    uri: "https://kube-cd.volkncloud.com/",
+
+    json: true,
+    body: data,
+    headers: {
+      "X-Hub-Signature": signature
+    }
+  },
+  (error, response, body) => {
+    if (error || response.statusCode < 200 || response.statusCode > 299) {
+      // Something went wrong
+      console.error(`Request failed with status code ${response.statusCode}!`);
+      console.error(response.body);
+
+      process.exit(1);
+    } else {
+      // Success
+      process.exit();
+    }
+  }
+);
